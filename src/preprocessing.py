5c3c2deb95da06cf3858c05622070a83546bf0b9
Prabhat Kulkarni <enterpriserka14@gmail.com>
2026-07-16 08:04:03 +0000

    Age missing handling

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 df = pd.read_csv("data/raw/train.csv")
 # Transform
 #Plan 1 to change Name into the Title
@@ -37,12 +38,44 @@ print(df["Family_Size"].value_counts())
 
 # Missing_value handelling
 from ml_utils import Missing_Summary
-print(Missing_Summary(df)[0])
+print(Missing_Summary(df))
 def checking_missing():
     Age = df["Age"].copy()
     #For median 
     median_age = Age.median()
-    Age = Age.fillna(round(median_age,0))
-    print(Age)
-    print(df.shape[0])
+    Median_Age = Age.fillna(round(median_age,0))
+    #Mean
+    mean_age = Age.mean()
+    Mean_Age = Age.fillna(round(mean_age))
+    #Zero
+    print(f"Skew for mean is added  :{Mean_Age.skew():.2f}")
+    print(f"Skew for median is added  :{Median_Age.skew():.2f}")
+    print(f"Actual Skew : {Age.skew():.2f}")
+    #histoogram for each
+    datasets = [
+    ("Original Age", Age),
+    ("Mean Filled", Mean_Age),
+    ("Median Filled", Median_Age)
+        ]
+
+    for title, data in datasets:
+
+
+        plt.figure(figsize=(8,4))
+        sns.histplot(data, kde=True)
+        plt.title(title)
+        plt.xlabel("Age")
+        plt.savefig(f"{title}_histogram.png")
+        plt.close()
+
+        plt.figure(figsize=(8,2))
+        sns.boxplot(x=data)
+        plt.title(title)
+        plt.savefig(f"{title}_boxplot.png")
+        plt.close()
+    print(Age.describe())
+
+    print(Mean_Age.describe())
+
+    print(Median_Age.describe())    
 checking_missing()
