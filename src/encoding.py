74052a252a336022c70f47c97e70e2289a0f2500
Prabhat Kulkarni <enterpriserka14@gmail.com>
2026-07-18 09:12:45 +0000

    Done with train.py , encoding.py

--- src/encoding.py
+++ src/encoding.py
@@ -1,9 +1,9 @@
-from sklearn.preprocessing import OneHotEncoder,OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
-def create_encoder_and_imputer():
+def scaler_transformer():
     categorical_features = ["Sex", "Embarked", "Title"]
     numerical_feature = ["Age"]
     categorical_pipeline = Pipeline(
@@ -15,7 +15,32 @@ def create_encoder_and_imputer():
 
     numerical_pipeline = Pipeline(
         steps=[
-        ("imputer", SimpleImputer(strategy="mean"))
+        ("imputer", SimpleImputer(strategy="mean")),
+        ("scaler" , StandardScaler())
+    ]
+    )
+    column_transformer = ColumnTransformer(
+        transformers=[
+        ("cat", categorical_pipeline,categorical_features ),
+        ("num", numerical_pipeline, numerical_feature )
+        ],
+        remainder="passthrough"
+    )
+
+    return column_transformer
+def non_scaler_transformer():
+    categorical_features = ["Sex", "Embarked", "Title"]
+    numerical_feature = ["Age"]
+    categorical_pipeline = Pipeline(
+        steps=[
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("encoder", OneHotEncoder(handle_unknown="ignore"))
+    ]
+    )
+
+    numerical_pipeline = Pipeline(
+        steps=[
+        ("imputer", SimpleImputer(strategy="mean")),
     ]
     )
     column_transformer = ColumnTransformer(
--- src/train.py
+++ src/train.py
@@ -1,4 +1,4 @@
-from encoding import create_encoder_and_imputer
+from encoding import non_scaler_transformer,scaler_transformer
 from preprocessing import preprocess
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -6,7 +6,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+from sklearn.svm import SVC
 import pandas as pd
 import numpy as np
 
@@ -18,7 +21,41 @@ df = preprocess(df)
 X = df.drop(columns="Survived")
 y = df["Survived"]
 
-X_train,y_train,X_test,y_test = train_test_split(X,y , random_state=42 , stratify= y , test_size= 0.2) 
+X_train, X_test, y_train, y_test = train_test_split(X,y , random_state=42 , stratify= y , test_size= 0.2) 
 
-transformer = create_encoder_and_imputer()
-transformer.fit_transform(X_train,y)
+#Descision Tree , Random forest , AdaBoost ,  XGBoost don't require Scalar 
+Non_scaler_models = {
+    "Decision Tree": DecisionTreeClassifier(),
+    "Random Forest": RandomForestClassifier(),
+    "AdaBoost": AdaBoostClassifier(),
+    "XGBoost": XGBClassifier(
+    ),
+    "ExtraTrees":ExtraTreesClassifier()
+}
+Scaler_models = {
+    "Logistic Classification":LogisticRegression(),
+    "KNN Classification":KNeighborsClassifier(),
+    "SVM Classification":SVC()
+}
+for name,model in Non_scaler_models.items():
+    models = Pipeline(
+        steps=
+        [
+           ( "preprocess",non_scaler_transformer()),
+            ("name" , model)
+        ]
+    
+    )
+    models.fit(X_train,y_train)
+    y_predict = models.predict(X_test)
+    
+for name , model in Scaler_models.items():
+    models = Pipeline(
+        steps=
+        [
+            ("preprocess",scaler_transformer()),
+            ("name" , model )
+        ]
+    )
+    models.fit(X_train,y_train)
+    y_predict=models.predict(X_test)
